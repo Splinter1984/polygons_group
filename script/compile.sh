274741515f99394ec38a32c6774993d087f5b611
Splinter1984 <rom.andrianov1984@gmail.com>
2021-10-08 10:50:14 +0700

    write error option

--- script/compile.sh
+++ script/compile.sh
@@ -1,22 +1,21 @@
 #! /bin/sh
 
+project_dir="broken_lines"
+source_dir=$(pwd)
+current_folder=$(pwd | grep -o '[^/]*$')
+
+if [[ $current_folder == "script" ]]; then
+    cd ../
+elif [[ $current_folder != "broken_lines" ]]; then
+    echo -e "\033[1;31m[ERROR]\033[0m: move file \033[100m${0##*/}\033[49m to the root of the project or '$project_dir/scripts' folder "
+    exit 1
+fi
+
 if [[ $OSTYPE == "msys" ]]; then
-    source_dir=$(pwd)
-    current_folder=$(pwd | grep -o '[^/]*$')
-    if [[ $current_folder == "script" ]]; then
-        cd ../
-    fi
     cmake -E make_directory build
     cd build
     cmake .. -G 'MinGW Makefiles' && mingw32-make
-
 elif [[ `$OSTYPE` == "linux-gnu" ]]; then
-    source_dir=$(pwd)
-    current_folder=$(pwd | grep -o '[^/]*$')
-    if [[ $current_folder == "script" ]]; then
-        cd ../
-    fi
     cmake -E make_directory build
     cd build && cmake .. && make
-
 fi
