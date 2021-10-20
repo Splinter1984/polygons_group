7d8d2a4e7d8e8d79470b0090d54c42ce1853cacd
Splinter1984 <rom.andrianov1984@gmail.com>
2021-10-21 00:22:25 +0800

    add some exception + debug mode

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,40 @@
 #include "segment2d.h"
 #include "polygon2d.h"
 
+#define BL_DEBUG
+
+/**
+ * the function allows you to read the boundary values for 
+ * a polygon and fill in the transmitted polygon vector
+ * @param `file` that contain polygons points, vector of store polygons
+ * @return stored polygons pointer 
+ */
 void read_lines(std::ifstream& file, std::vector<Polygon2D>* polygons)
 {
+    // init temp segments storage
     std::vector<Segment2D> polygon;
-    Segment2D* segment;
-    int startx, starty, endx, endy;
-    file >> startx >> starty;
-    int trigx, trigy;
+
+    /* init read values as float, cause python script
+        store value whith .0 */
+    float startx, starty, endx, endy;
+
+    // try to read two start values for set polygon triggers
+    try
+    {
+        if (!(file >> startx >> starty))
+            throw std::string("error to read values from file");
+    } catch (std::string& ex) {
+        std::cerr << ex << std::endl;
+        return;
+    }
+    
+    // init triggers for check, when polygon end
+    float trigx, trigy;
     trigx = startx;
     trigy = starty;
     while(file >> endx >> endy)
     {
-        segment = new Segment2D(startx, starty, endx, endy);
-        polygon.push_back(*segment);
-        std::cout << *segment << std::endl;
-        std::cout << polygon.back() << std::endl;
+        polygon.push_back(Segment2D((int)startx, (int)starty, (int)endx, (int)endy));
         if (trigx == endx && trigy == endy)
         {
             file >> startx >> starty;
@@ -55,6 +74,8 @@ int main()
     std::vector<Polygon2D> polygons;
     read_lines(file, &polygons);
     
+#ifdef BL_DEBUG
+
     size_t count = 1;
     for (const auto& polygon: polygons)
     {
@@ -63,6 +84,9 @@ int main()
             std::cout << *it << std::endl;
         count++;
     }
+
+#endif
+
     file.close();
 
     return 0;
